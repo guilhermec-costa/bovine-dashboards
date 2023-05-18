75db79061389b4c1b79c76f373740dbaf75021db
Guilherme China <echina725@gmail.com>
2023-05-18 17:41:27 -0300

    titulo legenda centralizado

--- modulos.py
+++ modulos.py
@@ -2,7 +2,7 @@
 
 def alter_legend(fig):
     fig.update_layout(legend=dict(font=dict(size=13, color="white"), bgcolor="#051732",
-                                  bordercolor="black", borderwidth=2, title=dict(text='Número da PLM', font=dict(size=16))))
+                                  bordercolor="black", borderwidth=2, title=dict(text=f'{" " * 15}Número da PLM', font=dict(size=16))))
     
 def alter_hover(fig, mode="closest"):
     fig.update_layout(hovermode=mode, hoverlabel=dict(bgcolor="AntiqueWhite", font_color="black",
