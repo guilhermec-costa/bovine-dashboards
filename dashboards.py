47c6931893237fc3549b1dc86be612b50e83215b
Guilherme China <echina725@gmail.com>
2023-05-18 13:59:18 -0300

    alterações feitas

--- dashboards.py
+++ dashboards.py
@@ -13,6 +13,13 @@ conc.dropna(axis=0, how='any', inplace=True)
 conc['Data correta'] = conc['Data correta'].apply(lambda x: datetime.strptime(x, '%d/%m/%Y %H:%M:%S'))
 agrupado = conc.groupby(by='PLM')
 
+def alter_legend(fig):
+    fig.update_layout(legend=dict(font=dict(size=13, color="white"), bgcolor="#051732",
+                                  bordercolor="black", borderwidth=2, title=dict(text='PLM dos bois', font=dict(size=16))))
+    
+def alter_hover(fig, mode="closest"):
+    fig.update_layout(hovermode=mode, hoverlabel=dict(bgcolor="AntiqueWhite", font_color="black",
+                                                    font_size=14, bordercolor="blue"))
 
 def multiplica(bateria):
     if bateria < 100:
@@ -24,14 +31,25 @@ for name, grupo in agrupado:
     grupo.sort_values(by='Data correta', inplace=True)
     grupo['BATERIA'] = grupo['BATERIA'].apply(lambda x: multiplica(x))
     grupo = grupo[grupo['Data correta'].dt.month > 4]
-    fig.add_trace(go.Scatter(x=grupo['Data correta'], y=grupo['BATERIA'], mode="markers+lines", line_shape='spline', name=name))
+    fig.add_trace(go.Scatter(x=grupo['Data correta'], y=grupo['BATERIA'], 
+                            mode="markers+lines", line_shape='spline', name=name, hovertemplate= f'<i>PLM: {name}</i>' + 
+                                                                                                '<br>Data: %{x}</br>' + 
+                                                                                                '<i>Bateria: %{y}</i>',
+                            ))
     # fig.add_trace(go.Scatter(x=grupo['Data correta'], y=grupo['BATERIA'], mode="markers+lines", line_shape='spline', legendgroup=name))
-fig.update_layout(width=2000, height=800, title=dict(text='Dashboards Spaceviz', xanchor='center',
-                                                     yanchor='top', x=0.5, y=0.93, font=dict(size=18)))
+fig.update_layout(height=900, title=dict(text='Dashboards SpaceVis', xanchor='center',
+                                                     yanchor='top', x=0.5, y=0.93, font=dict(size=25)))
+
 
 fig.update_yaxes(tickfont=dict(size=16), title=dict(
         text="Bateria", font=dict(size=16)))
 
+fig.update_xaxes(tickfont=dict(size=16), title=dict(font=dict(size=16)))
 
-st.write('')
+
+alter_legend(fig=fig)
+alter_hover(fig=fig)
+
+
+# st.date_input(label='Filtre a data:', min_value=conc['Data correta'].min())
 st.plotly_chart(fig, use_container_width=True)
