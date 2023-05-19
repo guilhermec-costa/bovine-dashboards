d9044282e8f2e4f8e249fcf72a52923e0392ceeb
Guilherme China <echina725@gmail.com>
2023-05-19 14:19:59 -0300

    conexao para a branch de conexao

--- dashboards.py
+++ dashboards.py
@@ -4,38 +4,51 @@ from datetime import datetime
 import streamlit as st
 import modulos
 import connection
+import psycopg2 as pgsql
 
 # configuração da página
 st.set_page_config(page_title='Dashboards SpaceVis', layout='wide', page_icon=':bar_chart:')
 
-conn = connection.start_connection()
+@st.cache_resource
+def start_connection():
+    return pgsql.connect(**st.secrets['postgres'])
+
+conn = start_connection()
 
 # Iniciando query
-@st.cache_data
+@st.cache_data(ttl=600)
 def run_query(query):
     with conn.cursor() as cursor:
-        cursor.execute(query=query)
+        cursor.execute(query)
         return cursor.fetchall()
     
 
-content = run_query("""
-SELECT * FROM public.BatteryView
+content = run_query('SELECT * FROM public."viewdashboards";')
+columns_name = run_query("""
+SELECT column_name FROM information_schema.columns
+WHERE table_schema = 'public' AND table_name   = 'viewdashboards';
 """)
 
+colunas = [x[0] for x in columns_name]
+df = pd.DataFrame(content, columns=colunas)
+# df['LastCommunication'] = df['LastCommunication'].apply(lambda x: datetime.strptime(x, '%d/%m/%Y %H:%M:%S%z'))
+st.write(df)
+
 # leitura dos dados
-conc = pd.read_excel('novo_concatenado.xlsx')
+conc = df
+# conc = pd.read_excel('novo_concatenado.xlsx')
 conc.dropna(axis=0, how='any', inplace=True)
 
 
 #conversão para datas
-conc['Data correta'] = conc['Data correta'].apply(lambda x: datetime.strptime(x, '%d/%m/%Y %H:%M:%S'))
+# conc['Data correta'] = conc['Data correta'].apply(lambda x: datetime.strptime(x, '%d/%m/%Y %H:%M:%S'))
 
 c1, c2 = st.columns(2)
 
 # filtro de ddtas
-inicio = c1.date_input(label='Data de ínicio:', min_value=conc['Data correta'].min(),
-                        key='data_inicio', value=datetime(2023, 5, 1))
-fim = c2.date_input(label='Data de fim:', min_value=conc['Data correta'].min(), 
+inicio = c1.date_input(label='Data de ínicio:', min_value=conc['CreatedAt'].min(),
+                        key='data_inicio', value=datetime(2023, 5, 14))
+fim = c2.date_input(label='Data de fim:', min_value=conc['CreatedAt'].min(), 
                     key='data_fim', value=datetime.now())
 
 
@@ -50,10 +63,10 @@ else:
 
 
 # Função que multiplica por 1000 os valores menores que 100.
-filtrado_plm['BATERIA'] = filtrado_plm['BATERIA'].apply(lambda x: modulos.multiplica(x))
+filtrado_plm['battery'] = filtrado_plm['battery'].apply(lambda x: modulos.multiplica(x))
 
-minimo = int(filtrado_plm['BATERIA'].min())
-maximo = int(filtrado_plm['BATERIA'].max())
+minimo = int(filtrado_plm['battery'].min())
+maximo = int(filtrado_plm['battery'].max())
 
 
 slider_bateria = st.slider(label='Range de bateria', value=[minimo, maximo],
@@ -61,12 +74,12 @@ slider_bateria = st.slider(label='Range de bateria', value=[minimo, maximo],
                         )
 
 # Função que filtra os dispostivos conforme o range de bateria selecionado no slider
-filtrado_plm = filtrado_plm[(filtrado_plm.BATERIA >= slider_bateria[0]) & (filtrado_plm.BATERIA <= slider_bateria[1])]
+filtrado_plm = filtrado_plm[(filtrado_plm.battery >= slider_bateria[0]) & (filtrado_plm.battery <= slider_bateria[1])]
 
 # Filtro de datas
-inicio = pd.to_datetime(inicio)
-fim = pd.to_datetime(fim)
-filtrado = filtrado_plm[(filtrado_plm['Data correta'] >= inicio) & (filtrado_plm['Data correta'] <= fim)]
+inicio = pd.to_datetime(inicio, utc=True)
+fim = pd.to_datetime(fim, utc=True)
+filtrado = filtrado_plm[(filtrado_plm['LastCommunication'] >= inicio) & (filtrado_plm['LastCommunication'] <= fim)]
 
 # Agrupamentos por PLM
 agrupado = filtrado.groupby(by='PLM')
@@ -74,9 +87,9 @@ fig = go.Figure()
 
 # Inserção de uma linha no gráfico para cada agrupamento de PLM
 for name, grupo in agrupado:
-    grupo.sort_values(by='Data correta', inplace=True)
-    grupo = grupo[grupo['Data correta'].dt.month > 4]
-    fig.add_trace(go.Scatter(x=grupo['Data correta'], y=grupo['BATERIA'], 
+    grupo.sort_values(by='LastCommunication', inplace=True)
+    grupo = grupo[grupo['LastCommunication'].dt.month > 4]
+    fig.add_trace(go.Scatter(x=grupo['LastCommunication'], y=grupo['battery'], 
                             mode="markers+lines", line_shape='spline', name=name, hovertemplate= f'<i>PLM: {name}</i>' + 
                                                                                                 '<br>Data: %{x}</br>' + 
                                                                                                 '<i>Bateria: %{y}</i>',
